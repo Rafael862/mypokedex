b8f7f96c17d9afb3a80e4f5de2741222a12842ee
Rafael Ferreira Gomes <ferreira.rafael862@gmail.com>
2022-09-29 21:47:03 -0300

    utilizei a fetch api para buscar dados, agora vou fazer a inse~ção de imagens de forma que o usuário escreva e envie no formulário

--- js/js.js
+++ js/js.js
@@ -1,9 +1,10 @@
-    function insert(){
+/*function insert(){
+
 
     let namepokemon = document.getElementById('inputpokemon').value;
     //console.log(namepokemon);
-    let imgpokemon = document.createElement("img");
-    let linkimg = document.createTextNode(`src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${namepokemon}.gif";`);
+    let imgpokemon = document.createElement("div");
+    let linkimg = document.create(`<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${namepokemon}.gif" alt="">`);
         imgpokemon.appendChild(linkimg);
         let body = document.querySelector('.imgpokemon');
         body.appendChild(imgpokemon);
@@ -12,7 +13,25 @@
 
 }
 
-document.getElementById('submit').onclick = insert;
+document.getElementById('submit').onclick = insert;*/
 
+const doGet = (url) => {
+    const promiseCallback = (resolve, reject) => {
+            fetch(url) // retorna uma promisse, então logo apos vem o then
+        .then((response) => { // o response contem algumas informações que vieram do fetch API  
+            if (!response.ok) throw  new Error('Erro ao executar requisição, status' + response.status);
+            return response.json();//retorna um objeto
+    })
+        .then(resolve)
+        .catch(reject);
+    }
+    return new Promise(promiseCallback);
+}
+
+var pokemon = 'ditto'
+doGet(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(console.log).catch(console.error);
+
+
+ 
 
 
