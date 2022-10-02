493dc96c3ecac48bbb5fb4534fc6bbf08ac8a313
Rafael Ferreira Gomes <ferreira.rafael862@gmail.com>
2022-10-02 11:02:17 -0300

    fiz aparecer o pokemon inserindo o nome ou número dele

--- js/js.js
+++ js/js.js
@@ -15,7 +15,7 @@
 
 document.getElementById('submit').onclick = insert;*/
 
-const doGet = (url) => {
+/*const doGet = (url) => {
     const promiseCallback = (resolve, reject) => {
             fetch(url) // retorna uma promisse, então logo apos vem o then
         .then((response) => { // o response contem algumas informações que vieram do fetch API  
@@ -23,6 +23,7 @@ const doGet = (url) => {
             return response.json();//retorna um objeto
     })
         .then(resolve)
+        .then(data)
         .catch(reject);
     }
     return new Promise(promiseCallback);
@@ -31,9 +32,39 @@ const doGet = (url) => {
 function submit (){
     var pokemon = document.getElementById('inputpokemon').value; //pego o que foi escrito e jogo dentro da variável
     doGet(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(console.log).catch(console.error); //aqui eu joguei dentro da url para pegar o pokemon desejado
-    var img = document.createElement("IMG");
-        img.src= `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-        document.body.appendChild(img);
+    //let img = data['sprites']['front_defaut'];
+    document.getElementById('pic').setAttribute('src', doGet);
 }
-document.getElementById('submit').addEventListener('click', submit);
+document.getElementById('submit').addEventListener('click', submit);*/
 
+ function submit() {
+
+    var pokemon = document.getElementById('inputpokemon').value;
+
+ console.log(pokemon);
+
+ 
+ 
+
+//let pokemon = 25;
+let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    fetch(url)
+    .then((response) =>{
+        return response.json();
+    })
+    .then((data) =>{
+        console.log(data);
+        document.querySelector('.namepokemon').innerHTML = data['name'];
+        let img = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        document.getElementById('pic').setAttribute('src', img);
+    })
+    .catch((erro) => {
+        console.log('Erro: ' + erro)
+    });
+
+}
+
+    
+    
+document.getElementById('submit').addEventListener('click', submit);
+    //document.getElementById('submit').onclick = loadapi;
